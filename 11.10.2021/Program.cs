d8df3674bce35ac2ce27e997bfe901b676eb8f7c
Soukal Filip <soukal.fi.2019@skola.ssps.cz>
2021-10-11 14:37:02 +0200

    Add project files.

---
+++ 11.10.2021/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToUpperCasenew toUpperCasenew = new ToUpperCasenew();
+        }
+    }
+    class ToUpperCasenew
+    {
+        public ToUpperCasenew()
+        {
+            bool uppercase = false;
+            char[] separators = {'<','>'};
+            string p = "He's a <upcase>real nowhere man</upcase>, sitting in his land.";
+            string[] field = p.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (field[i] == "/upcase")
+                {
+                    uppercase = false;
+                    field[i] = "";
+                }
+                if (uppercase == true)
+                {
+                    field[i] = field[i].ToUpper();
+                }
+                if (field[i] == "upcase")
+                {
+                    uppercase = true;
+                    field[i] = "";
+                }
+            }
+            for (int i = 0; i < field.Length; i++)
+            {
+                Console.Write(field[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
